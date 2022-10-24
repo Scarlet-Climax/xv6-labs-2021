f49aec4fa559b83ed98c4e673090ecd107261fe2
flandre <flandre@umich.edu>
2022-10-24 17:00:16 -0500

    barrier

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -12,6 +12,7 @@ struct barrier {
   pthread_cond_t barrier_cond;
   int nthread;      // Number of threads that have reached this round of the barrier
   int round;     // Barrier round
+  int done;
 } bstate;
 
 static void
@@ -20,6 +21,7 @@ barrier_init(void)
   assert(pthread_mutex_init(&bstate.barrier_mutex, NULL) == 0);
   assert(pthread_cond_init(&bstate.barrier_cond, NULL) == 0);
   bstate.nthread = 0;
+  bstate.done = 0;
 }
 
 static void 
@@ -30,7 +32,20 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  while (bstate.done)
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  bstate.nthread++;
+  if (bstate.nthread == nthread) {
+    bstate.round++;
+    bstate.done = 1;
+  }
+  pthread_cond_broadcast(&bstate.barrier_cond);
+  while (!bstate.done)
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  bstate.nthread--;
+  if (bstate.nthread == 0) bstate.done = 0;
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
